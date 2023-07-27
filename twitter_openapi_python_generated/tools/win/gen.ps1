57a0afe050831ad4b3025d449b28439f08c369e1
ふぁ <yuki@yuki0311.com>
2023-07-27 18:42:33 +0900

    update tools
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter_openapi_python_generated/tools/win/gen.ps1
+++ twitter_openapi_python_generated/tools/win/gen.ps1
@@ -1,8 +1,11 @@
 
+Move-Item "\\wsl.localhost\Ubuntu-22.04\home\yuki\openapi-generator\modules\openapi-generator-cli\target\openapi-generator-cli.jar" "tools/openapi-generator-cli.jar" -Force
+
+
 ./twitter-openapi/.venv/Scripts/Activate.ps1
 Start-Process -FilePath "python" -ArgumentList "tools/build.py" -WorkingDirectory "twitter-openapi" -Wait -NoNewWindow
 
 tools/win/clean.ps1
-java -jar tools/openapi-generator-cli.jar generate -g python-nextgen -c tools/openapi-generator-config.yaml --git-repo-id twitter_openapi_python --git-user-id fa0311
+java -jar tools/openapi-generator-cli.jar generate -g python -c tools/openapi-generator-config.yaml --git-repo-id twitter_openapi_python --git-user-id fa0311
 
-Start-Process -FilePath "python" -ArgumentList "tools/win/replace.py" -Wait -NoNewWindow
+# Start-Process -FilePath "python" -ArgumentList "tools/win/replace.py" -Wait -NoNewWindow
