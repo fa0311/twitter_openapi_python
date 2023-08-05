65c8964df983b5b48aeb2f6188b8350a2e6d053e
ふぁ <yuki@yuki0311.com>
2023-08-05 20:17:10 +0900

    add tweet api
    Signed-off-by: ふぁ <yuki@yuki0311.com>

---
+++ twitter_openapi_python/test/api/test_tweet_api.py
@@ -0,0 +1,69 @@
+import unittest
+import twitter_openapi_python as api
+from test.api import get_client, print_tweet
+
+
+class TestTweetApi(unittest.TestCase):
+    client: api.TweetApiUtils
+
+    def setUp(self):
+        self.client = get_client().get_tweet_api()
+
+    def test_get_tweet_detail(self):
+        result = self.client.get_tweet_detail(focal_tweet_id="1349129669258448897")
+        for tweet in result.data.data:
+            if tweet.promoted_metadata is not None:
+                continue
+            print_tweet(tweet)
+
+    def get_home_timeline(self):
+        result = self.client.get_home_timeline()
+        for tweet in result.data.data:
+            if tweet.promoted_metadata is not None:
+                continue
+            print_tweet(tweet)
+
+    def test_get_home_latest_timeline(self):
+        result = self.client.get_home_latest_timeline()
+        for tweet in result.data.data:
+            if tweet.promoted_metadata is not None:
+                continue
+            print_tweet(tweet)
+
+    def test_get_list_latest_tweets_timeline(self):
+        result = self.client.get_list_latest_tweets_timeline(
+            list_id="1141162794290520064"
+        )
+        for tweet in result.data.data:
+            if tweet.promoted_metadata is not None:
+                continue
+            print_tweet(tweet)
+
+    def test_get_user_tweets(self):
+        result = self.client.get_user_tweets(user_id="44196397")
+        for tweet in result.data.data:
+            print_tweet(tweet)
+
+    def test_get_user_tweets_and_replies(self):
+        result = self.client.get_user_tweets_and_replies(user_id="44196397")
+        for tweet in result.data.data:
+            print_tweet(tweet)
+
+    def test_get_user_media(self):
+        result = self.client.get_user_media(user_id="44196397")
+        for tweet in result.data.data:
+            print_tweet(tweet)
+
+    def test_get_likes(self):
+        result = self.client.get_likes(user_id="44196397")
+        for tweet in result.data.data:
+            print_tweet(tweet)
+
+    def test_get_bookmarks(self):
+        result = self.client.get_bookmarks()
+        for tweet in result.data.data:
+            print_tweet(tweet)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ twitter_openapi_python/twitter_openapi_python/api/tweet_api.py
@@ -0,0 +1,321 @@
+import twitter_openapi_python_generated as twitter
+
+import twitter_openapi_python_generated.models as models
+from twitter_openapi_python.models import (
+    ApiUtilsRaw,
+    TwitterApiUtilsResponse,
+    TweetListApiUtilsResponse,
+)
+from twitter_openapi_python.models.header import ApiUtilsHeader
+
+from twitter_openapi_python.utils.api import (
+    instruction_to_entry,
+    tweet_entries_converter,
+    entries_cursor,
+    build_response,
+)
+
+from typing import Any, Callable, Type, TypeVar, List, Optional
+import json
+
+T = TypeVar("T")
+
+
+class TweetApiUtils:
+    api: twitter.TweetApi
+    flag: dict[str, Any]
+
+    def __init__(self, api: twitter.PostApi, flag: dict[str, Any]):
+        self.api = api
+        self.flag = flag
+
+    def request(
+        self,
+        apiFn: Callable[[str, str, str], twitter.ApiResponse],
+        convertFn: Callable[[T], List[models.InstructionUnion]],
+        type: Type[T],
+        key: str,
+        param: dict[str, Any],
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        assert self.flag[key] is not None
+
+        res = apiFn(
+            self.flag[key]["queryId"],
+            json.dumps(self.flag[key]["variables"] | param),
+            json.dumps(self.flag[key]["features"]),
+        )
+        if isinstance(res.data.actual_instance, models.Errors):
+            errors: models.Errors = res.data.actual_instance
+            raise Exception(errors)
+        instruction = convertFn(res.data.actual_instance)
+        entry = instruction_to_entry(instruction)
+        tweet_list = tweet_entries_converter(entry)
+        raw = ApiUtilsRaw(
+            instruction=instruction,
+            entry=entry,
+        )
+        data = TweetListApiUtilsResponse(
+            raw=raw,
+            cursor=entries_cursor(entry),
+            data=tweet_list,
+        )
+
+        return build_response(
+            response=res,
+            data=data,
+            type=ApiUtilsHeader,
+        )
+
+    def get_tweet_detail(
+        self,
+        focal_tweet_id: str,
+        cursor: Optional[str] = None,
+        controller_data: Optional[str] = None,
+        extra_param: Optional[dict[str, Any]] = None,
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        param: dict[str, Any] = {
+            "focalTweetId": focal_tweet_id,
+        }
+        if cursor is not None:
+            param["cursor"] = cursor
+        if controller_data is not None:
+            param["controller_data"] = controller_data
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_tweet_detail_with_http_info,
+            convertFn=lambda e: e.data.threaded_conversation_with_injections_v2.instructions,
+            type=models.TweetDetailResponse,
+            key="TweetDetail",
+            param=param,
+        )
+        return response
+
+    def get_search_timeline(
+        self,
+        raw_query: str,
+        product: Optional[str] = None,
+        cursor: Optional[str] = None,
+        count: Optional[int] = None,
+        extra_param: Optional[dict[str, Any]] = None,
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        param: dict[str, Any] = {
+            "rawQuery": raw_query,
+        }
+        if product is not None:
+            param["product"] = product
+        if cursor is not None:
+            param["cursor"] = cursor
+        if count is not None:
+            param["count"] = count
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_search_timeline_with_http_info,
+            convertFn=lambda e: e.data.search_by_raw_query.search_timeline.timeline.instructions,
+            type=models.SearchTimelineResponse,
+            key="SearchTimeline",
+            param=param,
+        )
+        return response
+
+    def get_home_timeline(
+        self,
+        cursor: Optional[str] = None,
+        count: Optional[int] = None,
+        extra_param: Optional[dict[str, Any]] = None,
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        param: dict[str, Any] = {}
+        if cursor is not None:
+            param["cursor"] = cursor
+        if count is not None:
+            param["count"] = count
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_home_timeline_with_http_info,
+            convertFn=lambda e: e.data.home.home_timeline_urt.instructions,
+            type=models.TimelineResponse,
+            key="HomeTimeline",
+            param=param,
+        )
+        return response
+
+    def get_home_latest_timeline(
+        self,
+        cursor: Optional[str] = None,
+        count: Optional[int] = None,
+        extra_param: Optional[dict[str, Any]] = None,
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        param: dict[str, Any] = {}
+        if cursor is not None:
+            param["cursor"] = cursor
+        if count is not None:
+            param["count"] = count
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_home_latest_timeline_with_http_info,
+            convertFn=lambda e: e.data.home.home_timeline_urt.instructions,
+            type=models.TimelineResponse,
+            key="HomeLatestTimeline",
+            param=param,
+        )
+        return response
+
+    def get_list_latest_tweets_timeline(
+        self,
+        list_id: str,
+        cursor: Optional[str] = None,
+        count: Optional[int] = None,
+        extra_param: Optional[dict[str, Any]] = None,
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        param: dict[str, Any] = {
+            "listId": list_id,
+        }
+        if cursor is not None:
+            param["cursor"] = cursor
+        if count is not None:
+            param["count"] = count
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_list_latest_tweets_timeline_with_http_info,
+            convertFn=lambda e: e.data.list.tweets_timeline.timeline.instructions,
+            type=models.ListLatestTweetsTimelineResponse,
+            key="ListLatestTweetsTimeline",
+            param=param,
+        )
+        return response
+
+    def get_user_tweets(
+        self,
+        user_id: str,
+        cursor: Optional[str] = None,
+        count: Optional[int] = None,
+        extra_param: Optional[dict[str, Any]] = None,
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        param: dict[str, Any] = {
+            "userId": user_id,
+        }
+        if cursor is not None:
+            param["cursor"] = cursor
+        if count is not None:
+            param["count"] = count
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_user_tweets_with_http_info,
+            convertFn=lambda e: e.data.user.result.timeline_v2.timeline.instructions,
+            type=models.UserTweetsResponse,
+            key="UserTweets",
+            param=param,
+        )
+        return response
+
+    def get_user_tweets_and_replies(
+        self,
+        user_id: str,
+        cursor: Optional[str] = None,
+        count: Optional[int] = None,
+        extra_param: Optional[dict[str, Any]] = None,
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        param: dict[str, Any] = {
+            "userId": user_id,
+        }
+        if cursor is not None:
+            param["cursor"] = cursor
+        if count is not None:
+            param["count"] = count
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_user_tweets_and_replies_with_http_info,
+            convertFn=lambda e: e.data.user.result.timeline_v2.timeline.instructions,
+            type=models.UserTweetsResponse,
+            key="UserTweetsAndReplies",
+            param=param,
+        )
+        return response
+
+    def get_user_media(
+        self,
+        user_id: str,
+        cursor: Optional[str] = None,
+        count: Optional[int] = None,
+        extra_param: Optional[dict[str, Any]] = None,
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        param: dict[str, Any] = {
+            "userId": user_id,
+        }
+        if cursor is not None:
+            param["cursor"] = cursor
+        if count is not None:
+            param["count"] = count
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_user_media_with_http_info,
+            convertFn=lambda e: e.data.user.result.timeline_v2.timeline.instructions,
+            type=models.UserTweetsResponse,
+            key="UserMedia",
+            param=param,
+        )
+        return response
+
+    def get_likes(
+        self,
+        user_id: str,
+        cursor: Optional[str] = None,
+        count: Optional[int] = None,
+        extra_param: Optional[dict[str, Any]] = None,
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        param: dict[str, Any] = {
+            "userId": user_id,
+        }
+        if cursor is not None:
+            param["cursor"] = cursor
+        if count is not None:
+            param["count"] = count
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_likes_with_http_info,
+            convertFn=lambda e: e.data.user.result.timeline_v2.timeline.instructions,
+            type=models.UserTweetsResponse,
+            key="Likes",
+            param=param,
+        )
+        return response
+
+    def get_bookmarks(
+        self,
+        cursor: Optional[str] = None,
+        count: Optional[int] = None,
+        extra_param: Optional[dict[str, Any]] = None,
+    ) -> TwitterApiUtilsResponse[TweetListApiUtilsResponse, ApiUtilsHeader]:
+        param: dict[str, Any] = {}
+        if cursor is not None:
+            param["cursor"] = cursor
+        if count is not None:
+            param["count"] = count
+        if extra_param is not None:
+            param.update(extra_param)
+
+        response = self.request(
+            apiFn=self.api.get_bookmarks_with_http_info,
+            convertFn=lambda e: e.data.bookmark_timeline_v2.timeline.instructions,
+            type=models.BookmarksResponse,
+            key="Bookmarks",
+            param=param,
+        )
+        return response
