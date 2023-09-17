d0ec469bff6901d5f57c0925ecce973409dc7706
ふぁ <yuki@yuki0311.com>
2023-09-17 21:58:54 +0900

    update version 0.0.3
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter_openapi_python_generated/twitter_openapi_python_generated/__init__.py
+++ twitter_openapi_python_generated/twitter_openapi_python_generated/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 # import apis into sdk package
 from twitter_openapi_python_generated.api.default_api import DefaultApi
--- twitter_openapi_python_generated/twitter_openapi_python_generated/configuration.py
+++ twitter_openapi_python_generated/twitter_openapi_python_generated/configuration.py
@@ -468,7 +468,7 @@ conf = twitter_openapi_python_generated.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.0.1\n"\
-               "SDK Package Version: 0.0.2".\
+               "SDK Package Version: 0.0.3".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
