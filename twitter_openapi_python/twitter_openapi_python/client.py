1b205147a884656be5b8615fba0affe29ff07db4
ふぁ <yuki@yuki0311.com>
2023-09-28 13:29:19 +0900

    update
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter_openapi_python/twitter_openapi_python/client.py
+++ twitter_openapi_python/twitter_openapi_python/client.py
@@ -1,3 +1,5 @@
+import random
+import re
 from typing import Any
 
 import twitter_openapi_python_generated as twitter
@@ -56,44 +58,103 @@ class TwitterOpenapiPythonClient:
 
 
 class TwitterOpenapiPython:
-    hash: str = "2d477a0fb84d249a30b5af535b467efc25b34923"
+    hash: str = "7560ee63488ec9d15f5389e64867f2413701d7dd"
     placeholder_url = "https://raw.githubusercontent.com/fa0311/twitter-openapi/{hash}/src/config/placeholder.json"
     placeholder: ParamType
     user_agent: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+    access_token: str = "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
 
-    def get_twitter_openapi_python_client(self, api: twitter.ApiClient):
+    api_key = {
+        "Accept": "*/*",
+        "AcceptEncoding": "gzip, deflate, br",
+        "AcceptLanguage": "en-US,en;q=0.9",
+        "CacheControl": "no-cache",
+        "Pragma": "no-cache",
+        "SecChUa": '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+        "SecChUaMobile": "?0",
+        "SecChUaPlatform": '"Windows"',
+        "SecFetchDest": "empty",
+        "SecFetchMode": "cors",
+        "SecFetchSite": "same-origin",
+        "ClientLanguage": "en",
+        "ActiveUser": "yes",
+    }
+
+    browser_headers = {
+        "accept": "text/plain, */*; q=0.01",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "pragma": "no-cache",
+        "sec-ch-ua": '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "user-agent": user_agent,
+    }
+
+    def cookie_normalize(self, cookie: list[str]) -> dict[str, str]:
+        return {
+            x.split("; ")[0].split("=")[0]: x.split("; ")[0].split("=")[1]
+            for x in cookie
+        }
+
+    def cookie_to_str(self, cookie: dict[str, str]) -> str:
+        return "; ".join([f"{key}={value}" for key, value in cookie.items()])
+
+    def get_twitter_openapi_python_client(
+        self,
+        api: twitter.ApiClient,
+    ) -> TwitterOpenapiPythonClient:
         http = urllib3.PoolManager()
         flag = http.request("GET", self.placeholder_url.format(hash=self.hash)).json()
         return TwitterOpenapiPythonClient(api, flag)
-
-    def get_client(self, api_client: twitter.ApiClient) -> TwitterOpenapiPythonClient:
-        return self.get_twitter_openapi_python_client(api_client)
 
     def get_client_from_cookies(
         self,
         cookies: dict[str, str],
     ) -> TwitterOpenapiPythonClient:
-        api_conf = conf.Configuration(
-            api_key={
-                "Accept": "*/*",
-                "AcceptEncoding": "gzip, deflate, br",
-                "AcceptLanguage": "en-US,en;q=0.9",
-                "CacheControl": "no-cache",
-                "Pragma": "no-cache",
-                "SecChUa": '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-                "SecChUaMobile": "?0",
-                "SecChUaPlatform": '"Windows"',
-                "SecFetchDest": "empty",
-                "SecFetchMode": "cors",
-                "SecFetchSite": "same-origin",
-                "ClientLanguage": "en",
-                "ActiveUser": "yes",
-                "AuthType": "OAuth2Session",
-                "CsrfToken": cookies.get("ct0", ""),
-            },
+        api_key = self.api_key
+
+        if cookies.get("ct0"):
+            api_key.update({"AuthType": "OAuth2Session"})
+        ct0 = cookies.get("ct0", "".join(random.choices("0123456789abcdef", k=32)))
+        api_key.update({"CsrfToken": ct0})
+
+        gt = cookies.get("gt")
+        if gt is not None:
+            api_key.update({"GuestToken": gt})
+
+        api_conf = conf.Configuration(api_key=api_key)
+        api_conf.access_token = self.access_token
+        api_client = twitter.ApiClient(
+            configuration=api_conf,
+            cookie=self.cookie_to_str(cookies),
         )
-        api_conf.access_token = "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
-        str_cookie = "; ".join([f"{key}={value}" for key, value in cookies.items()])
-        api_client = twitter.ApiClient(configuration=api_conf, cookie=str_cookie)
         api_client.user_agent = self.user_agent
         return self.get_twitter_openapi_python_client(api_client)
+
+    def get_guest_client(self) -> TwitterOpenapiPythonClient:
+        http = urllib3.PoolManager()
+        session = {}
+
+        res_1 = http.request(
+            "GET",
+            "https://twitter.com",
+            redirect=False,
+            headers=self.browser_headers,
+        )
+        cookie = res_1.headers._container["set-cookie"][1:]
+        session.update(self.cookie_normalize(cookie))
+        res_2 = http.request(
+            "GET",
+            "https://twitter.com",
+            redirect=False,
+            headers=self.browser_headers | {"Cookie": self.cookie_to_str(session)},
+        )
+
+        find = re.findall(r'document.cookie="(.*?)";', res_2.data.decode())
+        session.update(self.cookie_normalize(find))
+        return self.get_client_from_cookies(session)
