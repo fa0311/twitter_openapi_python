d23e9535acb4cc071687ac016b2b9a10b7b44bdc
ふぁ <yuki@yuki0311.com>
2023-11-21 12:08:07 +0900

    update gen
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter_openapi_python_generated/tools/win/gen.ps1
+++ twitter_openapi_python_generated/tools/win/gen.ps1
@@ -2,8 +2,8 @@
 # Move-Item "\\wsl.localhost\Ubuntu-22.04\home\yuki\openapi-generator\modules\openapi-generator-cli\target\openapi-generator-cli.jar" "tools/openapi-generator-cli.jar" -Force
 
 
-./twitter-openapi/.venv/Scripts/Activate.ps1
-Start-Process -FilePath "python" -ArgumentList "tools/build.py" -WorkingDirectory "twitter-openapi" -Wait -NoNewWindow
+# ./twitter-openapi/.venv/Scripts/Activate.ps1
+# Start-Process -FilePath "python" -ArgumentList "tools/build.py" -WorkingDirectory "twitter-openapi" -Wait -NoNewWindow
 
 tools/win/clean.ps1
 java -jar tools/openapi-generator-cli.jar generate -g python -c tools/openapi-generator-config.yaml --git-repo-id twitter_openapi_python --git-user-id fa0311
