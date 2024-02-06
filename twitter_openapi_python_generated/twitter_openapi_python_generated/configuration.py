a3233fb4171ee7427b7b4890d6e7723872888b52
ふぁ <yuki@yuki0311.com>
2024-02-06 15:10:51 +0900

    update version
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter_openapi_python_generated/twitter_openapi_python_generated/configuration.py
+++ twitter_openapi_python_generated/twitter_openapi_python_generated/configuration.py
@@ -567,7 +567,7 @@ conf = twitter_openapi_python_generated.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.0.1\n"\
-               "SDK Package Version: 0.0.8".\
+               "SDK Package Version: 0.0.9".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
