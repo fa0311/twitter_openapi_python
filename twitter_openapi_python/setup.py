7939e1203b5605da558e59833780d917111c9deb
ふぁ <yuki@yuki0311.com>
2025-05-12 01:25:48 +0900

    update communityId
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter_openapi_python/setup.py
+++ twitter_openapi_python/setup.py
@@ -5,7 +5,7 @@ import re
 from setuptools import find_packages, setup
 
 NAME = "twitter_openapi_python"
-VERSION = "0.0.37"
+VERSION = "0.0.38"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = ["twitter_openapi_python_generated == 0.0.33", "pydantic >= 2.6", "urllib3 >= 2.1.0, < 3.0.0"]
 GITHUB_RAW_URL = (
