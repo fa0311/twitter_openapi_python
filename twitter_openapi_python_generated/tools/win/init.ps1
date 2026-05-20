469668415d2c7c984262c71062a3833927a35a6f
fa0311 <yuki@yuki0311.com>
2026-05-21 01:48:28 +0900

    update version 0.0.34

--- twitter_openapi_python_generated/tools/win/init.ps1
+++ twitter_openapi_python_generated/tools/win/init.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -OutFile tools/openapi-generator-cli.jar https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.12.0/openapi-generator-cli-7.12.0.jar
+Invoke-WebRequest -OutFile tools/openapi-generator-cli.jar https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.22.0/openapi-generator-cli-7.22.0.jar
