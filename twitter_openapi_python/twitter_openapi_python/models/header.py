a29a66c350e3a5fd5891ab3570ca1ae274f3b5f2
fa0311 <yuki@yuki0311.com>
2026-05-21 02:12:43 +0900

    update 0.0.43

--- twitter_openapi_python/twitter_openapi_python/models/header.py
+++ twitter_openapi_python/twitter_openapi_python/models/header.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from pydantic import Field
 
@@ -7,14 +7,14 @@ from twitter_openapi_python.models import BaseModel
 
 class ApiUtilsHeader(BaseModel):
     raw: Dict[str, str] = Field()
-    connection_hash: str = Field()
-    content_type_options: str = Field()
-    frame_options: str = Field()
+    connection_hash: Optional[str] = Field()
+    content_type_options: Optional[str] = Field()
+    frame_options: Optional[str] = Field()
     response_time: int = Field()
     tfe_preserve_body: bool = Field()
-    transaction_id: str = Field()
-    xss_protection: int = Field()
+    transaction_id: Optional[str] = Field()
+    xss_protection: Optional[int] = Field()
     rate_limit_limit: int = Field()
     rate_limit_remaining: int = Field()
     rate_limit_reset: int = Field()
-    twitter_response_tags: str = Field()
+    twitter_response_tags: Optional[str] = Field()
